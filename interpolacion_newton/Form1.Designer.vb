9560acf75fa62beed8e6f178ffed8423b08ee924
AkulaFoulke <alejandrocarrillo06@hotmail.com>
2021-05-19 19:26:51 -0500

    primer commit

---
+++ interpolacion_newton/Form1.Designer.vb
@@ -0,0 +1,457 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.tvx = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tn = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Salida1 = New System.Windows.Forms.DataGridView()
+        Me.Salir = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.tc = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Salida2 = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Gráfica = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.índice = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.Salida1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Salida2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Gráfica, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(174, 320)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(89, 17)
+        Me.Label5.TabIndex = 184
+        Me.Label5.Text = "Interpolación"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(301, 320)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(163, 22)
+        Me.TextBox2.TabIndex = 183
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(254, 174)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 17)
+        Me.Label4.TabIndex = 182
+        Me.Label4.Text = "ib ="
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(301, 174)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(163, 22)
+        Me.TextBox1.TabIndex = 181
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.White
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button2.Location = New System.Drawing.Point(515, 130)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 29)
+        Me.Button2.TabIndex = 180
+        Me.Button2.Text = "&Graficar"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'tvx
+        '
+        Me.tvx.Location = New System.Drawing.Point(301, 78)
+        Me.tvx.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.tvx.Name = "tvx"
+        Me.tvx.Size = New System.Drawing.Size(163, 22)
+        Me.tvx.TabIndex = 178
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(78, 83)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(188, 17)
+        Me.Label3.TabIndex = 177
+        Me.Label3.Text = "Dame el número a interpolar"
+        '
+        'tn
+        '
+        Me.tn.Location = New System.Drawing.Point(301, 23)
+        Me.tn.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.tn.Name = "tn"
+        Me.tn.Size = New System.Drawing.Size(163, 22)
+        Me.tn.TabIndex = 175
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(83, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(183, 17)
+        Me.Label2.TabIndex = 174
+        Me.Label2.Text = "Dame el número de parejas"
+        '
+        'Salida1
+        '
+        Me.Salida1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Salida1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Salida1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.índice, Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7, Me.Column8})
+        Me.Salida1.Location = New System.Drawing.Point(23, 362)
+        Me.Salida1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Salida1.Name = "Salida1"
+        Me.Salida1.RowHeadersWidth = 62
+        Me.Salida1.RowTemplate.Height = 28
+        Me.Salida1.Size = New System.Drawing.Size(664, 269)
+        Me.Salida1.TabIndex = 172
+        '
+        'Salir
+        '
+        Me.Salir.BackColor = System.Drawing.Color.White
+        Me.Salir.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Salir.Location = New System.Drawing.Point(515, 248)
+        Me.Salir.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Salir.Name = "Salir"
+        Me.Salir.Size = New System.Drawing.Size(100, 26)
+        Me.Salir.TabIndex = 171
+        Me.Salir.Text = "&Salir"
+        Me.Salir.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button1.Location = New System.Drawing.Point(515, 189)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 26)
+        Me.Button1.TabIndex = 170
+        Me.Button1.Text = "&Limpiar"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'tc
+        '
+        Me.tc.Location = New System.Drawing.Point(301, 130)
+        Me.tc.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.tc.Name = "tc"
+        Me.tc.Size = New System.Drawing.Size(163, 22)
+        Me.tc.TabIndex = 168
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(78, 135)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(145, 17)
+        Me.Label1.TabIndex = 167
+        Me.Label1.Text = "Dame cifras signitivas"
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.White
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button3.Location = New System.Drawing.Point(515, 74)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 29)
+        Me.Button3.TabIndex = 185
+        Me.Button3.Text = "&Calcular"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Salida2
+        '
+        Me.Salida2.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Salida2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Salida2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2})
+        Me.Salida2.Location = New System.Drawing.Point(733, 476)
+        Me.Salida2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Salida2.Name = "Salida2"
+        Me.Salida2.RowHeadersWidth = 62
+        Me.Salida2.RowTemplate.Height = 28
+        Me.Salida2.Size = New System.Drawing.Size(454, 162)
+        Me.Salida2.TabIndex = 186
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.HeaderText = "coeficientes"
+        Me.DataGridViewTextBoxColumn1.MinimumWidth = 8
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.Width = 150
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.HeaderText = "deltas k"
+        Me.DataGridViewTextBoxColumn2.MinimumWidth = 8
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.Width = 150
+        '
+        'Gráfica
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Gráfica.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Gráfica.Legends.Add(Legend1)
+        Me.Gráfica.Location = New System.Drawing.Point(711, 42)
+        Me.Gráfica.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Gráfica.Name = "Gráfica"
+        Series1.ChartArea = "ChartArea1"
+        Series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point
+        Series1.Color = System.Drawing.Color.Chartreuse
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Series2.ChartArea = "ChartArea1"
+        Series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point
+        Series2.Color = System.Drawing.Color.Red
+        Series2.Legend = "Legend1"
+        Series2.Name = "Series2"
+        Me.Gráfica.Series.Add(Series1)
+        Me.Gráfica.Series.Add(Series2)
+        Me.Gráfica.Size = New System.Drawing.Size(490, 416)
+        Me.Gráfica.TabIndex = 187
+        Me.Gráfica.Text = "Chart1"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(254, 215)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(31, 17)
+        Me.Label6.TabIndex = 189
+        Me.Label6.Text = "m ="
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(301, 215)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(163, 22)
+        Me.TextBox3.TabIndex = 188
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.ForeColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(254, 255)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(27, 17)
+        Me.Label7.TabIndex = 191
+        Me.Label7.Text = "s ="
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(301, 255)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(163, 22)
+        Me.TextBox4.TabIndex = 190
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.White
+        Me.Button6.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Button6.Location = New System.Drawing.Point(490, 20)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(156, 28)
+        Me.Button6.TabIndex = 192
+        Me.Button6.Text = "&Introducir parejas"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'índice
+        '
+        Me.índice.HeaderText = "i"
+        Me.índice.MinimumWidth = 8
+        Me.índice.Name = "índice"
+        Me.índice.Width = 90
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "x(i)(Δk 0)"
+        Me.Column1.MinimumWidth = 8
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 150
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "y(i)(Δk 0)"
+        Me.Column2.MinimumWidth = 8
+        Me.Column2.Name = "Column2"
+        Me.Column2.Width = 150
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Δ1"
+        Me.Column3.MinimumWidth = 8
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 90
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Δ2"
+        Me.Column4.MinimumWidth = 8
+        Me.Column4.Name = "Column4"
+        Me.Column4.Width = 90
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Δ3"
+        Me.Column5.MinimumWidth = 8
+        Me.Column5.Name = "Column5"
+        Me.Column5.Width = 90
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Δ4"
+        Me.Column6.MinimumWidth = 8
+        Me.Column6.Name = "Column6"
+        Me.Column6.Width = 90
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "Δ5"
+        Me.Column7.MinimumWidth = 8
+        Me.Column7.Name = "Column7"
+        Me.Column7.Width = 90
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "Δ6"
+        Me.Column8.MinimumWidth = 8
+        Me.Column8.Name = "Column8"
+        Me.Column8.Width = 90
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1349, 718)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Gráfica)
+        Me.Controls.Add(Me.Salida2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.tvx)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.tn)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Salida1)
+        Me.Controls.Add(Me.Salir)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.tc)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Salida1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Salida2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Gráfica, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents tvx As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents tn As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Salida1 As DataGridView
+    Friend WithEvents Salir As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents tc As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Salida2 As DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents Gráfica As DataVisualization.Charting.Chart
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents índice As DataGridViewTextBoxColumn
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+End Class
---
+++ interpolacion_newton/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("interpolacion_newton")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("interpolacion_newton")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("428f7a84-c373-4a3e-8749-3b2b43625318")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
